a34fbc4332cbb775c43cc4a6a8ae8e6d5fd5cc9b
Athens410 <athenah.coucean@gmail.com>
2023-06-06 17:52:59 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,34 @@ function App() {
        <Weather defaultCity="Toronto"/>
 
 </div>
-       
+<div className="end">
+   This project was coded by
+          <a href="https://www.delac.io/" 
+          rel="noreferrer"
+          target="_blank">
+            Athenah Coucean
+          </a>
+          and is
+          <a
+            href="https://github.com/Athens410/react-final.git"
+            rel="noreferrer"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+          and
+          <a
+            href="https://main--stunning-nasturtium-43eab0.netlify.app/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            hosted on Netlify
+          </a>
+       </div>
+   
+   
+   
+   
     <ul className="nav justify-content-center mt-5">
   <li className="nav-item">
     <a className="nav-link active" href="https://www.shecodes.io/" target="_blank" rel="noreferrer" title="SheCodes profile">
