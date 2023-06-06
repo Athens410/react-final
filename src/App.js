768c026b02c125e4fa76c889adbfc5590d11f1c0
Athens410 <athenah.coucean@gmail.com>
2023-06-06 13:46:50 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div className="border">
-
+         <button type="button" class="btn btn-primary">Primary</button>
 
 
         </div>
+      <ul class="nav justify-content-center mt-5">
+    <li class="nav-item">
+        <a class="nav-link active" href="https://www.shecodes.io/" target="_blank" rel="noreferrer" title="SheCodes profile">SheCodes</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#"><i class="fa-brands fa-instagram" title="Instagram"  rel="noreferrer"></i></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="https://linkedin.com/in/athenah-c-2639251a0" target="_blank" rel="noreferrer" title="linkedin profile"> <i class="fa-brands fa-linkedin"></i></a>
+    </li>
+<li>
+    <a class="nav-link" href="https://github.com/Athens410/Portfolio-Project-Git.git" target="_blank" title="github profile" rel="noreferrer">   <i class="fa-brands fa-github"></i></a>
+    </li>
+
+
+
+
+    <li class="nav-item ml-5">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">© 2021 SheCodes All rights reserved.</a>
+    </li>
+    
+    
+</ul>
+
+<div class="rue"> 
+<h3>⚡️👩🏽‍💻This website was coded by, Athenah Coucean</h3>
+</div>
+
         
       </header>
     </div>
