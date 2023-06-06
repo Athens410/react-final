391a248ca6ee29df05a2e812f4cace5bd370b554
Athens410 <athenah.coucean@gmail.com>
2023-06-06 14:02:05 -0400

    added font awesome

--- src/App.js
+++ src/App.js
@@ -12,29 +12,26 @@ function App() {
 
 
         </div>
-         <ul className="nav justify-content-center mt-5">
-      <li className="nav-item">
-        <a className="nav-link active" href="https://www.shecodes.io/" target="_blank" title="SheCodes profile">
-          SheCodes
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">
-          <i className="fab fa-instagram" title="Instagram"></i>
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="https://linkedin.com/in/athenah-c-2639251a0" target="_blank" title="linkedin profile">
-          <i className="fab fa-linkedin"></i>
-        </a>
-      </li>
-      <li>
-        <a className="nav-link" href="https://github.com/Athens410/Portfolio-Project-Git.git" target="_blank" title="github profile">
-          <i className="fab fa-github"></i>
-        </a>
-      </li>
-    </ul>
-  
+    <ul className="nav justify-content-center mt-5">
+  <li className="nav-item">
+    <a className="nav-link active" href="https://www.shecodes.io/" target="_blank" rel="noreferrer" title="SheCodes profile">
+      SheCodes
+    </a>
+  </li>
+  <li className="nav-item">
+   
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="https://linkedin.com/in/athenah-c-2639251a0" target="_blank" rel="noreferrer" title="LinkedIn profile">
+      <i className="fab fa-linkedin"></i>
+    </a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="https://github.com/Athens410/Portfolio-Project-Git.git" target="_blank" rel="noreferrer" title="GitHub profile">
+      <i className="fab fa-github"></i>
+    </a>
+  </li>
+</ul>
 
 
         
